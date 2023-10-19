0c1a48328812e0ad7bd6f2b7fbf243ab77a91cdc
FilipBednarek <131236640+FilipBednarek@users.noreply.github.com>
2023-10-19 11:12:22 +0200

    Add files via upload

---
+++ kalk-wyborczy/main.js
@@ -0,0 +1,7 @@
+const btnAdd = document.querySelector('#btn');
+const przyciski = document.querySelectorAll('input[type="button"]');
+
+przyciski.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('jhagjsh');
+});
